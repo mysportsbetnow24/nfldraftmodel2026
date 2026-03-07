2bbb9ec969f1d8d5c84ec7eaae63a94bf96ace9e
Nick Holz <nickholz@mac.home>
2026-03-06 22:05:25 -0500

    Add crawler controls and rights page

--- astro-site/src/pages/robots.txt.ts
+++ astro-site/src/pages/robots.txt.ts
@@ -5,6 +5,28 @@ export const GET: APIRoute = ({ site }) => {
   const body = [
     "User-agent: *",
     "Allow: /",
+    "Content-signal: search=yes, ai-train=no, ai-input=no",
+    "",
+    "User-agent: GPTBot",
+    "Disallow: /",
+    "",
+    "User-agent: ClaudeBot",
+    "Disallow: /",
+    "",
+    "User-agent: Google-Extended",
+    "Disallow: /",
+    "",
+    "User-agent: CCBot",
+    "Disallow: /",
+    "",
+    "User-agent: Bytespider",
+    "Disallow: /",
+    "",
+    "User-agent: Amazonbot",
+    "Disallow: /",
+    "",
+    "User-agent: Applebot-Extended",
+    "Disallow: /",
     "",
     `Sitemap: ${base}/sitemap.xml`,
     ""
@@ -16,4 +38,3 @@ export const GET: APIRoute = ({ site }) => {
     }
   });
 };
-
